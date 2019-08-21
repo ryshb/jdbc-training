1425727e11b040dc89bf1cfbde8c1e268ce9e22f
Rishabh Gautam <ryshbgautam@gmail.com>
2019-08-21 07:18:46 +0000

    Aug 21 2019

--- JdbcProject/src/main/java/com/visa/training/jdbc/ProductApp.java
+++ JdbcProject/src/main/java/com/visa/training/jdbc/ProductApp.java
@@ -6,7 +6,7 @@ public class ProductApp {
 
 	public static void main(String[] args) {
 		JdbcProductDAO dao = new JdbcProductDAO();
-		Product test = new Product("test", 1999, 100);
+		Product test = new Product("test",1999, 100);
 		Product saved = dao.save(test);
 		System.out.println("created product with id : "+saved.getId());
 		System.out.println("__________ALL________");
---
+++ productapp/src/main/java/com/visa/prj/dao/PersistenceException.java
@@ -0,0 +1,30 @@
+package com.visa.prj.dao;
+
+public class PersistenceException extends Exception {
+
+	public PersistenceException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenceException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PersistenceException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
